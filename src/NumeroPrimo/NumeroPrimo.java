3aaab67110eecdbad86b1eaf54937b6fdf9ececf
ALUMNOS <ALUMNOS@P606>
2022-05-16 09:36:40 +0200

    segundo commit

--- src/NumeroPrimo/NumeroPrimo.java
+++ src/NumeroPrimo/NumeroPrimo.java
@@ -5,28 +5,12 @@ public class NumeroPrimo {
 
 	public static void main(String[] args) { 		
 		
-		Scanner num = new Scanner(System.in);
-		int contador,numero;
+	
     
 	    System.out.print("Buenos días Ceinmark, soy Fernando Gomez \r\n"
 	    		+ "\r\n"
 	    		+ "Introduce un número para averiguar si es primo o no:\r\n"
 	    		+ " ");
-	    numero = num.nextInt();
-	    contador = 0;
-	
-	    for(int i = 1; i <= numero; i++)    {
-	        if((numero % i) == 0)
-	        {
-	            contador++;
-	        }
-	    }	
-	    if(contador <= 2)
-	    {
-	        System.out.println("El numero es primo");
-	    }else{
-	        System.out.println("El numero no es primo");
-	    }
 
 	}
 
